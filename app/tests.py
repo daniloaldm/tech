4339bf6a98d5812ccaa6a1ae77a801aa8fde0975
Danilo Alexandrino <danilo.alexandrinodm@gmail.com>
2020-07-16 21:51:10 -0300

    Adicionando testes

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APITestCase
 
 # Create your tests here.
+class ApiTest(APITestCase):
+    def test_list_processors(self):
+        request = self.client.get("/processors/")
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(request.data), 4)
+
+    def test_list_memories(self):
+        request = self.client.get("/rammemorys/")
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(request.data), 5)
+
+    def test_list_mother_board(self):
+        request = self.client.get("/motherboards/")
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(request.data), 3)
+
+    def test_list_video_board(self):
+        request = self.client.get("/videocards/")
+
+        self.assertEqual(request.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(request.data), 3)
+
